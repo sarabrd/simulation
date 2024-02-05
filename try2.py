9a23ddedb4fb4c8d8cddd07dd4ae78a5d174813b
sarabrd <87406295+sarabrd@users.noreply.github.com>
2024-02-05 00:52:12 +0000

    think one works? with combining tables

--- try2.py
+++ try2.py
@@ -3,9 +3,7 @@
 num_tables = 3
 table_sizes =   [1, 2, 3]
 arrival_time =  [10, 13, 4, 5, 11, 9, 15]
-#arrival_time =  [10, 13, 5, 5, 11, 9, 15]
 groups =        [3, 2, 3, 2, 1, 1, 3]
-#stay_time =     [17, 15, 20, 30, 12, 25, 25]
 stay_time =     [17, 15, 21, 30, 12, 25, 25]
 profit =        [25, 15, 30, 17, 7, 10, 20]
 current_arrival_index = 0
@@ -19,13 +17,8 @@ T_plus = Termination + 1
 EC =  [T_plus, T_plus, T_plus, arrival_time[current_arrival_index], 60] #event calendar: departure1, departure2, departure3, arrival, termination
 
 while TNOW <= Termination:
-    #print("-----------------------")    
-    #print("tnow [", TNOW, "]")#, "tnext [", TNEXT, "]")
-    #print("b",B)
-    #print("EC", EC)
     print(TNOW, ",",TNEXT, ",", B, ",",EC)
     minimum_value = min(EC)
-    #TNOW = minimum_value
 
     #first, departure happens
     if minimum_value in (EC[0], EC[1], EC[2]):
@@ -35,7 +28,6 @@ while TNOW <= Termination:
                 B[i] = 0
                 TNOW = EC[i]
                 EC[i] = T_plus
-                #break    
     
     # if the next thing that happens is an arrival
     elif minimum_value == EC[3]: #and minimum_value not in (EC[0], EC[1], EC[2]):
@@ -47,6 +39,12 @@ while TNOW <= Termination:
                 B[i] = 1
                 EC[i] = EC[3] + stay_time[current_arrival_index]
                 break
+
+            #combining tables
+            elif B[0] == 0 and B[1] == 0 and B[2] == 1 and groups[current_arrival_index] == 3:
+                B[0] = B[1] = 1
+                EC[0] = EC[1] = EC[3] + stay_time[current_arrival_index]
+
         #schedule a new arrival  
         EC[3] += arrival_time[current_arrival_index+1]  
         current_arrival_index += 1
@@ -55,13 +53,6 @@ while TNOW <= Termination:
     else:
         print("terminantion")
         break
-
-    #if EC[3] in (EC[0], EC[1], EC[2]):
-    #    TNOW = TNEXT
-
-    #TNOW = TNEXT
-    #TNEXT = min(EC)
-    #TNOW = min(EC)
 
     
 
